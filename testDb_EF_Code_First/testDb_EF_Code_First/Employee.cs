1c9c12fdd30f3b23060287eb1e1beebbcced8376
Akash-Rana-25 <akashrana.work.gmail.com>
2023-05-17 14:40:22 +0530

    Code first apporch

---
+++ testDb_EF_Code_First/testDb_EF_Code_First/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDb_EF_Code_First
+{
+    [Table("Employee")]
+    internal class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Employee_PK { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int EmpCode { get; set; }
+        [Required]
+        public int Gender { get; set; }
+        [Required]
+        public System.DateTime DOB { get; set; }
+        public Nullable<decimal> Salary { get; set; }
+        [Required]
+        public System.DateTime JoiningDate { get; set; }
+        public Nullable<System.DateTime> ResignDate { get; set; }
+    }
+}
---
+++ testDb_EF_Code_First/testDb_EF_Code_First/ICrudOpration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDb_EF_Code_First
+{
+    internal interface ICrudOpration
+    {
+        void Add(Employee employee);
+        void Update(int Id);
+        void Delete(int Id);
+        void Read();
+    }
+}
